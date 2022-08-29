32178aa1dcb92fb3b3a03cb3b44d561f1fc2fbb6
denynsk <d.katalanin@ngs.ru>
2022-08-29 20:20:32 +0700

    app3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
